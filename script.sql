0a02a6307e7b10eb157192b27569cffbe0c31e3f
naamalevi1357 <naamalevi1357@gmail.com>
2025-01-19 14:07:54 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,3 +1,46 @@
+---1
+
+-- 1
+SELECT Courses.*, Lecturers.name from Courses
+INNER JOIN Lecturers on Courses.lecturer_id= Lecturers.lecturer_id 
+
+--2
+SELECT Courses.*, Lecturers.name from Courses
+LEFT JOIN Lecturers on Courses.lecturer_id= Lecturers.lecturer_id 
+
+-- 3
+SELECT Courses.*, Lecturers.name from Courses
+right JOIN Lecturers on Courses.lecturer_id= Lecturers.lecturer_id 
+
+-- 4
+SELECT Courses.*, Lecturers.name from Courses
+LEFT JOIN Lecturers on Courses.lecturer_id= Lecturers.lecturer_id
+WHERE Courses.lecturer_id is null
+
+-- 5
+--It didn't work for me, and there is a lecturer 1 who is not assigned to the course
+--I tried several possibilities - but without success
+
+SELECT Courses.*, Lecturers.name from Courses
+right JOIN Lecturers on Courses.lecturer_id= Lecturers.lecturer_id 
+WHERE Lecturers.lecturer_id is null
+
+SELECT Lecturers.*, Courses.name from Lecturers
+LEFT JOIN Courses on Lecturers.lecturer_id= Courses.lecturer_id
+WHERE Lecturers.lecturer_id is null
+
+-- 8
+SELECT * FROM Lecturers WHERE name like '%i%';
+
+-- 9
+SELECT count(*) FROM Lecturers ;
+
+-- 10
+SELECT count(*) FROM Courses ;
+
+---2
+
+
 -- 1
 SELECT Employees.*,Departments.name,Departments.budget, Departments.building  FROM Employees
 INNER JOIN Departments on Employees.department_id=Departments.department_id 
